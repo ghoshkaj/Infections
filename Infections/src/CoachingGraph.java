d86f92a5820699c1f15b888cbd7a7296071dab5a
ghoshkaj <kghosh1@students.towson.edu>
2014-10-21 22:54:07 -0400

    DFS for Total Infection

--- Infections/src/CoachingGraph.java
+++ Infections/src/CoachingGraph.java
@@ -3,7 +3,7 @@
  */
 
 /**
- * @author kg
+ * @author Kajari Ghosh
  *
  */
 
@@ -55,7 +55,7 @@ public class CoachingGraph {
 		int totalUsers, userID; // info for set up to read in graph
 		int user1, user2; // info for creating edges between users		
 		
-		totalUsers = input.nextInt() + 1;
+		totalUsers = input.nextInt() + 1; // so that when using totalUsers as end point for iterating i, i matches with userList index
 		
 		//initialize all users in user vertex
 		for(int i = 1; i < totalUsers; i++){
@@ -87,7 +87,7 @@ public class CoachingGraph {
 //		return input.nextInt();
 //	}
 	
-	public void printGraph(){
+	public void printDetailedGraph(){
 		
 		printGraph(userList, totalUsers);
 	}
@@ -99,7 +99,7 @@ public class CoachingGraph {
 		for(int i = 1; i < totalUsers; i++){
 			//print out user name and site version
 			System.out.print("User: " + userList[i].UserID);
-			System.out.println("\tSite Version: " + userList[i].UserID);
+			System.out.println("\tSite Version: " + userList[i].siteVersion);
 			System.out.print("Coaches/coached by: " );
 			
 			//go into the linked list and print out members here
@@ -114,13 +114,78 @@ public class CoachingGraph {
 		
 	}
 	
-//	private void 
+public void printSiteVersionGraph(){
+		
+		printSiteVersionGraph(userList, totalUsers);
+	}
+	
+	private void printSiteVersionGraph(User[] userList, int totalUsers){
+		
+		Coaches temp;
+
+		for(int i = 1; i < totalUsers; i++){
+			//print out user name and site version
+			System.out.print("User: " + userList[i].UserID);
+			System.out.println("\tSite Version: " + userList[i].siteVersion);
+		}
+		
+	}
+	
+
+	//public driver for recursive DFS totalInfection method
+	public void totalInfection(int userID, int newSiteVersion){
+		boolean[] visited = new boolean[totalUsers];
+		totalInfectionDFS(userID, visited, newSiteVersion);
+	}
+	
+	//totalInfection using recursive depth first search
+	private void totalInfectionDFS(int userID, boolean[] visited, int siteVersion){
+		visited[userID] = true;
+		userList[userID].siteVersion = siteVersion;
+		Coaches temp = userList[userID].adjacencyList;
+		
+		while(temp != null){
+			if(!visited[temp.UserID]){
+				totalInfectionDFS(temp.UserID, visited, siteVersion);
+			}	
+			temp = temp.next;
+		}
+	}
+	
+	//COUNTER FOR QUESTION TWO
+//	public void DFS(){
+//		boolean[] visited = new boolean[totalUsers];
+//		for(int userID = 1; userID < totalUsers; userID++){
+//			if(!visited[userID]){
+//				DFS(userID, visited);
+//			}
+//		}
+//	
+//	}
+//	
+//	//totalInfection using iterative depth first search
+//	private void DFS(int userID, boolean[] visited){
+//		visited[userID] = true;
+//		Coaches temp = userList[userID].adjacencyList;
+//		
+//		while(temp != null){
+//			if(!visited[temp.UserID]){
+//				DFS(temp.UserID, visited);
+//			}	
+//			temp = temp.next;
+//		}
+//	}
+	
+	
 	
 	public static void main(String[] args) throws FileNotFoundException {
 			
 		CoachingGraph newGraph = new CoachingGraph("/home/kg/git/Infections/src/input.txt");
 		
-		newGraph.printGraph();
+		newGraph.printSiteVersionGraph();
+		//userID, siteVersion
+		newGraph.totalInfection(3,3);
+		newGraph.printSiteVersionGraph();
 
 	}
 
